5ddf16964c04e5648f97fd7c0e61d6d9f6747e9f
Anirban Chakraborty <anirban.ch.it@gmail.com>
2023-04-18 23:26:43 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,1 @@
-terraform {
-  required_providers {
-    github = {
-      source = "integrations/github"
-      version = "5.23.0"
-    }
-  }
-}
 
-provider "github" {
-  # Configuration options
-}
