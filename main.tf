9edf2e09ea994085243e3e61d18e42b8c83ca9f1
Anirban Chakraborty <anirban.ch.it@gmail.com>
2023-04-18 23:06:58 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,13 @@ terraform {
     }
   }
 }
+
+variable token{
+  type = string
+  default = "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"
+}
+
+provider "github" {
+  token = var.token # or `GITHUB_TOKEN`
+}
+
